f2a86ff636e92c326b8c409044fa30b5dd325e5c
Hasse <hanneshermansson0000@gmail.com>
2018-11-03 22:48:03 +0100

    adjusted production scripts

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ cd "$(dirname "$0")"
 git fetch --all
 git checkout --force $1
 
-docker-compose -f docker-compose.yml -f docker/docker-compose.prod.yml build
+docker-compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml build
 
 docker-compose down
-docker-compose -f docker-compose.yml -f docker/docker-compose.prod.yml run django python manage.py makemigrations
-docker-compose -f docker-compose.yml -f docker/docker-compose.prod.yml run django python manage.py migrate
-docker-compose -f docker-compose.yml -f docker/docker-compose.prod.yml run django python manage.py collectstatic --noinput
+docker-compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml run django python manage.py makemigrations
+docker-compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml run django python manage.py migrate
+docker-compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml run django python manage.py collectstatic --noinput
 
-docker-compose -f docker-compose.yml -f docker/docker-compose.prod.yml up -d
+docker-compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml up -d
 
 sleep 2
 wget --retry --retry-connrefused http://localhost/debug/ok
