d2c71783d4aff9267fd2ef93a9f2f18b88691f66
Hasse <hanneshermansson0000@gmail.com>
2018-11-04 09:38:20 +0100

    typo of a typo

--- api/api/settings.py
+++ api/api/settings.py
@@ -121,7 +121,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/site_build/static')
+    os.path.join(BASE_DIR, '/site_build')
 ]
 
 # Celery setup
