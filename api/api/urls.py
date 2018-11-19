ce92dd7995e52d79be4328ac1a760a8df0199380
Hasse <hanneshermansson0000@gmail.com>
2018-11-19 17:39:14 +0100

    adjusted react app regex

--- api/api/urls.py
+++ api/api/urls.py
@@ -19,7 +19,6 @@ urlpatterns = [
 
 # Production react app proxy
 if os.environ.get("DJANGO_ENV", "development") == "production":
-    urlpatterns.insert(
-        len(urlpatterns),
+    urlpatterns.append(
         re_path('^.*', TemplateView.as_view(template_name='index.html'))
     )
