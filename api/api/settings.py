579586ea1ab80e60b1025a67a63bb2cb3cd5828b
Hasse <hanneshermansson0000@gmail.com>
2018-12-16 09:46:42 +0100

    fixed translation settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -163,8 +163,8 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
@@ -180,7 +180,7 @@ LOGGING = {
     },
     'loggers': {
         'django': {
-            'handlers':['file'],
+            'handlers': ['file'],
             'propagate': True,
             'level': 'DEBUG',
         },
